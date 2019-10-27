79597095b2773609c3ce19526c0603274c33ea17
aadityacr7 <aadityasaraiya25@gmail.com>
2019-10-28 01:52:50 +0530

    Added top N matches exrtaction

--- scripts/sfm_main.py
+++ scripts/sfm_main.py
@@ -1,10 +1,23 @@
 # To Do:
 '''
+Must have features 
+1) Filtering between matches (how do you quantify how good a match is)
+2) Loading calibration parameters from the file 
+3) Multiple image matching pipeline (aka choose good and bad images based on geometric checks)
+
+Good to have features 
 1) Implement Lowe's ratio test
-2) Implement multiple image matching pipeline
-3) Implement FLANN based Approximate nearest neighbor matching pipeline 
-4) Create separate classes for Stereo SFM and Monocular SFM  
+2) Implement FLANN based Approximate nearest neighbor matching pipeline 
+3) Create separate classes for Stereo SFM and Monocular SFM  
 '''
+
+
+
+# Argoverse data
+#from argoverse.map_representation.map_api import ArgoverseMap
+#from argoverse.data_loading.argoverse_tracking_loader import ArgoverseTrackingLoader
+#from argoverse.data_loading.argoverse_forecasting_loader import ArgoverseForecastingLoader
+
 
 import cv2
 import matplotlib.pyplot as plt
@@ -12,6 +25,7 @@ import numpy as np
 import sys
 import os
 import json 
+
 
 class Utils:
     
@@ -36,8 +50,8 @@ class Utils:
     # Check if there are images in the source path. If yes, push all the image paths in list and return 
     # Returns: Sorted list of image paths
     def extractImagePaths(self, sourcePath):
-        imagePaths= []
-        filenames = sorted(os.listdir(sourcePath))
+        imagePaths = []
+        filenames  = sorted(os.listdir(sourcePath))
 
         # If the filename is an image, append the path of the image 
         for filename in filenames:
@@ -48,16 +62,19 @@ class Utils:
     
     # Reads a .json file and returns the data from the .json file 
     def readJsonFile(self, jsonFilePath):
-        with open(jsonFilePath) as json_file:
-            data = json.load(json_file)
+        if os.path.exists(jsonFilePath):
+            with open(jsonFilePath,'r') as f:
+                try:
+                    data = json.load(f)
+                except Exception as e:
+                    print("got %s on json.load()" % e)
         return data
-
 
 class CVUtils():
     
     # Read an image from given path 
     def readImage(self, imPath):
-        img= cv2.imread(imPath)
+        img = cv2.imread(imPath)
         return img
     
     # Display and image with given window name 
@@ -69,11 +86,11 @@ class CVUtils():
 
     # Convert image to grayscale 
     def convertToGray(self, img):
-        gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         return gray
 
     # Detect features and descriptors from given image 
-    def featureDetection(self, img, descriptorType, display= False):
+    def featureDetection(self, img, descriptorType, topNMatches, display= False):
         # Initialize feature detector object 
         if descriptorType == "orb":
             featureDetector= cv2.ORB_create()
@@ -85,10 +102,10 @@ class CVUtils():
             featureDetector= cv2.xfeatures2d.SURF_create()
         
         # Convert image to grayscale 
-        gray= self.convertToGray(img)
+        gray = self.convertToGray(img)
 
         # Find keypoints and descriptors 
-        kp, des= featureDetector.detectAndCompute(img, None)
+        kp,des = featureDetector.detectAndCompute(img, None)
 
         # Display the image if flag
         if display:
@@ -98,88 +115,105 @@ class CVUtils():
         return [kp, des]
 
     # Find the matches between two images 
-    def featureMatching(self, img1, img2, descriptorType, displayMatches= False, displayKp= False):
+    def featureMatching(self, img1, img2, descriptorType, topNMatches, displayMatches= False, displayKp= False):
 
         # Find features and descriptors from given images 
-        [kp1, des1]= self.featureDetection(img1, descriptorType, displayKp)
-        [kp2, des2]= self.featureDetection(img2, descriptorType, displayKp)
+        [kp1, des1] = self.featureDetection(img1, descriptorType, displayKp)
+        [kp2, des2] = self.featureDetection(img2, descriptorType, displayKp)
+
+        print (len(kp1))
+        input("Press Enter to continue: ")
 
         if descriptorType == "sift" or descriptorType == "surf":
-            matchCriteria= cv2.NORM_L2
+            matchCriteria = cv2.NORM_L2
         
         elif descriptorType == "brisk" or descriptorType == "orb":
-            matchCriteria= cv2.NORM_HAMMING
+            matchCriteria = cv2.NORM_HAMMING
 
-        # create BFMatcher object
-        bf = cv2.BFMatcher(matchCriteria, crossCheck= True)
+        # Match features.
+        print('Matching features using KNN based matcher')
+        indexParams = dict(algorithm=0, trees=5)
+        searchParams = dict(checks=50)
+        flann = cv2.FlannBasedMatcher(indexParams, searchParams)
+        matches = flann.knnMatch(np.float32(des1), np.float32(des2), k=2)
+        # Store all the good matches as per Lowe's ratio test.
+        goodMatches = np.array([m for m, n in matches if m.distance < 0.7 * n.distance])
+        # Sorted matches 
+        sortedMatches= sorted(goodMatches, key = lambda x:x.distance)
+        print ("Before taking top N matches: ", len(sortedMatches))
+        # Removing the top N matches 
+        matches= sortedMatches[:min(len(sortedMatches), topNMatches)]
+        print ("After taking top N matches: ", len(matches))
 
-        # Match descriptors.
-        matches = bf.match(des1 , des2)
-
-        # Sort them in the order of their distance.
-        matches = sorted(matches, key = lambda x:x.distance)
-        
         # Display the matches if flag is true 
         if displayMatches:
-
-            if(len(matches)> 100):
-                print ("Number of matches more than 100. Drawing top 100 matches")
-                img3= cv2.drawMatches(img1,kp1,img2,kp2, matches[:100], None, flags=2)
-        
-            else:
-                print ("Drawing all matches")
-                img3= cv2.drawMatches(img1,kp1,img2,kp2, matches, None, flags=2)
-
+            print ("Drawing all matches")
+            img3=  cv2.drawMatches(img1,kp1,img2,kp2, matches, None, flags=2)
             plt.imshow(img3), plt.title(descriptorType), plt.show()
-        
         return matches 
-
-
 
 def main():
       
     # Utils object for OpenCV based tasks 
-    cvUtils= CVUtils() 
+    cvUtils = CVUtils() 
     # Utils object for general tasks 
-    utils= Utils()
+    utils = Utils()
     
+    # Loading Argo Map API objects 
+    #avm = ArgoverseMap()
+    #avTrackerLoader = ArgoverseTrackingLoader('argoverse-tracking/')    #simply change to your local path of the data
+    #avForecastingLoader = ArgoverseForecastingLoader('argoverse-forecasting/') #simply change to your local path of the data
+
     ################# Parsing input arguments ############
     # Path to Argoverse data folder 
-    dataFolderPath= sys.argv[1]
+    dataFolderPath = sys.argv[1]
     # Mode to run the SFM algorithm in: Stereo or Monocular 
-    sfmMode= sys.argv[2]
+    sfmMode = sys.argv[2]
     # Descriptor type for the matching process 
-    descriptorType= sys.argv[3]
+    descriptorType = sys.argv[3]
+    # Top N matches 
+    topNMatches= int(sys.argv[4])
     # Path to calibration config json file 
-    calibConfFilePath= dataFolderPath+ "/vehicle_calibration_info.json"
+    calibConfFilePath = dataFolderPath+ "/vehicle_calibration_info.json"
     # Path to city details json file 
-    cityConfigFilePath= dataFolderPath+ "/city_info.json"
+    cityConfigFilePath = dataFolderPath+ "/city_info.json"
 
     ############### Loading image data from data folder ######
-
-    cityName= utils.readJsonFile(cityConfigFilePath)['city_name']
+    
+    # Confirming that we are using Pittsburgh data 
+    cityName = utils.readJsonFile(cityConfigFilePath)['city_name']
     if(cityName!= "PIT"):
         print ("Data does not belong to Pittsburgh")
         return -1 
     else:
         print ("We have Pittsburgh data")
 
+    # Varying the data loading and calibration depending on the mode 
     if sfmMode == "stereo":
         print ("SFM in stereo mode")
 
         # Path to the stereo front left and front right folders
-        stereoFrFolder=  dataFolderPath + "/stereo_front_right"
-        stereoFlFolder= dataFolderPath + "/stereo_front_left" 
-
+        stereoFrFolder = dataFolderPath + "/stereo_front_right"
+        stereoFlFolder = dataFolderPath + "/stereo_front_left" 
         # Paths to all the images in respective folders 
-        stereoFrImgPaths=   utils.extractImagePaths(stereoFrFolder)
-        stereoFlImgPaths=   utils.extractImagePaths(stereoFlFolder)
+        stereoFrImgPaths = utils.extractImagePaths(stereoFrFolder)
+        stereoFlImgPaths = utils.extractImagePaths(stereoFlFolder)
 
-    imgR= cvUtils.readImage(stereoFrImgPaths[0])
-    imgL= cvUtils.readImage(stereoFlImgPaths[0])
+        #Calib dictionary for right and left camera      
+        stereoFrCalib= utils.readJsonFile(calibConfFilePath)['camera_data_'][7]
+        stereoFlCalib= utils.readJsonFile(calibConfFilePath)['camera_data_'][6]
+
+        print (stereoFrCalib)
+        #stereoFlCalib= camData['image_raw_stereo_front_right']
+
+
+    #TO DO: Loop through all images and perform exhaustive matching and add all 3D information to a global list 
+  
+    imgR = cvUtils.readImage(stereoFrImgPaths[0])
+    imgL = cvUtils.readImage(stereoFlImgPaths[0])
     
     # Extract matches from the first two images from the stereo folder 
-    matches= cvUtils.featureMatching(imgR, imgL, descriptorType, displayMatches= True, displayKp= False)
+    matches= cvUtils.featureMatching(imgR, imgL, descriptorType, topNMatches, displayMatches=True, displayKp=False)
 
 
 
--- scripts/sfm_main.sh
+++ scripts/sfm_main.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ $# -eq 3 ] 
+if [ $# -eq 4 ] 
 then
     echo "Received required args"
 else
     echo "Invalid args: arg1: Path to Data Folder, 
                         arg2: Mode (Options: mono, stereo), 
-                        arg3: Descriptor type (Options: sift, surf, brisk, orb) "
+                        arg3: Descriptor type (Options: sift, surf, brisk, orb)
+                        arg4: Top n matches"
+
     exit -1
 fi
 
-python sfm_main.py "$1" $2 $3
+python3 sfm_main.py "$1" $2 $3 $4
